a24855591227fdc18eeeb23cfb34884af06767c9
migue <miguel-gutierrez@hotmail.de>
2021-11-14 13:15:48 +0100

    for docker

--- src/main/java/com/frozzenList/controllers/UserController.java
+++ src/main/java/com/frozzenList/controllers/UserController.java
@@ -3,12 +3,15 @@ package com.frozzenList.controllers;
 import com.frozzenList.exceptions.UserNotFoundException;
 import com.frozzenList.models.User;
 import com.frozzenList.responetories.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
@@ -18,21 +21,30 @@ public class UserController {
 
     private final UserRepo userRepo;
 
+    @Autowired
     public UserController(UserRepo userRepo) {
         this.userRepo = userRepo;
     }
 
     @GetMapping("/users")
-    public List<User> allUsers() {
-        return userRepo.findAll();
+    public CollectionModel<EntityModel<User>> getAllUsers() {
+        List<EntityModel<User>> users = userRepo.findAll().stream()
+                .map((user) -> EntityModel.of(user,
+                        linkTo(methodOn(UserController.class).
+                                getUserById(user.getId())).
+                                withRel("users"))).
+                collect(Collectors.toList());
+        return CollectionModel.of(users,
+                linkTo(methodOn(UserController.class).
+                        getAllUsers()).withSelfRel());
     }
 
     @GetMapping("/users/{id}")
-    public EntityModel<User> userById(@PathVariable Long id) {
+    public EntityModel<User> getUserById(@PathVariable Long id) {
         User user = userRepo.findById(id).orElseThrow(() -> new UserNotFoundException(id));
         return EntityModel.of(user,
-                linkTo(methodOn(UserController.class).userById(id)).withSelfRel(),
-                linkTo(methodOn(UserController.class).allUsers().sel))
+                linkTo(methodOn(UserController.class).getUserById(id)).withSelfRel(),
+                linkTo(methodOn(UserController.class).getAllUsers()).withRel("users"));
     }
 
 
--- src/main/java/com/frozzenList/exceptions/UserNotFoundException.java
+++ src/main/java/com/frozzenList/exceptions/UserNotFoundException.java
@@ -2,6 +2,6 @@ package com.frozzenList.exceptions;
 
 public class UserNotFoundException extends RuntimeException {
     public UserNotFoundException(Long id) {
-        super("Could not find any User");
+        super("Could not find any id ");
     }
 }
